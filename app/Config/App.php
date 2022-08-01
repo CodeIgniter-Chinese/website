966c7879a419e0f3f4eb592f5eecdf9a93d2a8a8
Hex <hex@codeigniter.org.cn>
2022-08-01 13:17:06 +0800

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://codeigniter.org.cn';
+    public $baseURL = 'http://localhost:8080/';
 
     /**
      * --------------------------------------------------------------------------
