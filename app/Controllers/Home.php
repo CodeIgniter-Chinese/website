03c01cdaacdbfbf164e661f6323006d58203aaf4
Hex <hex@codeigniter.org.cn>
2022-08-01 22:45:04 +0800

    update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,28 +9,28 @@ class Home extends BaseController
         $news = [
             [
                 'title' => 'CodeIgniter 4.0 正式版发布了！',
-                'url' => 'https://zhuanlan.zhihu.com/p/108894797',
-                'date' => '2020.02.24',
+                'url'   => 'https://zhuanlan.zhihu.com/p/108894797',
+                'date'  => '2020.02.24',
             ],
             [
                 'title' => 'CodeIgniter 使用技巧系列文章',
-                'url' => 'https://zhuanlan.zhihu.com/p/68800595',
-                'date' => '2019.06.12',
+                'url'   => 'https://zhuanlan.zhihu.com/p/68800595',
+                'date'  => '2019.06.12',
             ],
             [
                 'title' => 'CodeIgniter 4 alpha 1 版发布了',
-                'url' => 'https://zhuanlan.zhihu.com/p/45641550',
-                'date' => '2018.09.29',
+                'url'   => 'https://zhuanlan.zhihu.com/p/45641550',
+                'date'  => '2018.09.29',
             ],
             [
                 'title' => 'CodeIgniter 4 中文手册翻译计划',
-                'url' => 'https://zhuanlan.zhihu.com/p/28911539',
-                'date' => '2017.08.30',
+                'url'   => 'https://zhuanlan.zhihu.com/p/28911539',
+                'date'  => '2017.08.30',
             ],
             [
                 'title' => 'CodeIgniter 4 Milestone 1 版发布了',
-                'url' => 'https://zhuanlan.zhihu.com/p/21482815',
-                'date' => '2016.07.04',
+                'url'   => 'https://zhuanlan.zhihu.com/p/21482815',
+                'date'  => '2016.07.04',
             ],
         ];
 
